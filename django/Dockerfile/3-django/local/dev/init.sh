4b0b3cfe73e4dd3f6cb5cbf1318e7e6afb4fa628
jeson_peng <jeson.peng@gmail.com>
2016-10-19 06:10:01 +0800

    fix init.sh

--- django/Dockerfile/3-django/local/dev/init.sh
+++ django/Dockerfile/3-django/local/dev/init.sh
@@ -11,7 +11,7 @@ sed -i "$ a STATIC_ROOT\ =\ os.path.join(BASE_DIR,\ 'static')" localomd/settings
 
 sed -i 's/django.db.backends.sqlite3/django.db.backends.mysql/1' localomd/settings.py
 sed -i "s/os.path.join(BASE_DIR, 'db.sqlite3')/'omd'/1" localomd/settings.py
-sed -i "/'omd',/a\ \ \ \ 'USER': 'root'," proj/settings.py
+sed -i "/'omd',/a\ \ \ \ 'USER': 'root'," localomd/settings.py
 sed -i "/'USER': 'root',/a\ \ \ \ 'PASSWORD': 'pjsong'," localomd/settings.py
 sed -i "/'PASSWORD': 'pjsong',/a\ \ \ \ 'HOST': 'mysql-omd-local'," localomd/settings.py
 
